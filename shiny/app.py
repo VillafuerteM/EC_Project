37dd3ca3be97950d96fa08bd914a3d2592194701
YunPerez <yuneri.perezao@gmail.com>
2023-12-03 00:31:13 -0600

    Modelo Python y Docker con Posgres

--- shiny/app.py
+++ shiny/app.py
@@ -1,59 +1,34 @@
-from flask import Flask, request, jsonify
-import joblib
-import pandas as pd
+from flask import Flask, request
+import json
+import psycopg2
+import psycopg2.extras
+import os
+
+# Estructura del uri:
+# "motor://user:password@host:port/database"
+database_uri = f'postgresql://{os.environ["PGUSR"]}:{os.environ["PGPASS"]}@{os.environ["PGHOST"]}:5432/{os.environ["PGDB"]}'
 
 app = Flask(__name__)
+conn = psycopg2.connect(database_uri)
 
-# Cargar el modelo entrenado
-model = joblib.load("path/to/your/xgboost_model.pkl")  # Asegúrate de proporcionar la ruta correcta
 
-@app.route('/predict', methods=['POST'])
-def predict():
+def execute_query(query, conn=conn):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
     try:
-        # Obtener los datos de la solicitud
-        data = request.get_json(force=True)
-        
-        # Convertir los datos a un DataFrame de pandas
-        input_data = pd.DataFrame(data)
-        
-        # Realizar la predicción
-        prediction = model.predict(input_data)
-        
-        # Enviar la respuesta
-        return jsonify({'prediction': prediction.tolist()})
-    
+        cur.execute(query)
+        results = cur.fetchall()
     except Exception as e:
-        return jsonify({'error': str(e)})
+        print(f"Error on the query: {e}")
+        results = []
+    cur.close()
+    return json.dumps([x._asdict() for x in results], default=str)
 
-if __name__ == '__main__':
-    app.run(port=5000)
+
+@app.route("/winequality", methods=["GET"])
+def winequality():
+    query = "select * from winequality limit 100"
+    return execute_query(query)
 
 
-#_________________________
-
-from flask import Flask, request, jsonify
-import xgboost
-import pandas as pd
-import numpy as np
-
-app = Flask(__name__)
-
-# Cargar el modelo XGBoost previamente entrenado
-model = xgboost.Booster()
-model.load_model("xgboost_model.rds")
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    features = pd.DataFrame(data)
-    
-    # Realizar la predicción
-    predictions = model.predict(xgboost.DMatrix(features))
-    
-    # Convertir las predicciones a la forma original (añadir 3 ya que restamos 3 antes)
-    predictions = predictions + 3
-    
-    return jsonify(predictions.tolist())
-
-if __name__ == '__main__':
-    app.run(port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8080)
